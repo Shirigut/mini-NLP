b52c8dd5219a86890fc7d67356435a4fa0b74096
unknown <shiriguti@gmail.com>
2022-01-12 16:11:30 +0200

    parse xml- function to parse xml of bible books

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ import xml.etree.ElementTree as ET
 def parse_xml(file_name):
     mytree = ET.parse(file_name)
     myroot = mytree.getroot()
-    psukim = myroot[1][0][0]
+    book = myroot[1][0]
+    counter = 0
+    words = {}
+    for perek in book:
+        for p in perek:
+            print(p.attrib['DisplayName_Heb'])
+            for word in p:
+                if word.text is not None and not word in words:
+                    words[counter] = word.text
+                    counter += 1
+    print(words)
+
 
 if __name__ == '__main__':
     parse_xml('Genesis.xml')
