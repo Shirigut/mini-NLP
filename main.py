61a42b59fa0f10afe8ff4ac4a483bbd6a397b7bf
unknown <shiriguti@gmail.com>
2022-01-13 13:56:35 +0200

    added books and wrote the hashmap to a file

--- main.py
+++ main.py
@@ -1,20 +1,52 @@
+import csv
+import os
+import pathlib
 import xml.etree.ElementTree as ET
 
+words = {}
 
-def parse_xml(file_name):
+def parse_xml(file_name, words, counter):
     mytree = ET.parse(file_name)
     myroot = mytree.getroot()
     book = myroot[1][0]
-    counter = 0
-    words = {}
     for perek in book:
         for p in perek:
             for word in p:
-                if word.text is not None and not word in words:
+                if word.text is not None and not word.text in words.values():
                     words[counter] = word.text
                     counter += 1
+    return words, counter
 
 
 
 if __name__ == '__main__':
-    parse_xml('Genesis.xml')
+    words = {}
+    counter = 0
+
+    for path in pathlib.Path("Torah").iterdir(): ##torah
+        if path.is_file():
+            words_new, counter_new = parse_xml(path, words, counter)
+            words = words_new
+            counter = counter_new
+
+    for path in pathlib.Path("Prophets").iterdir(): ##neviim
+        if path.is_file():
+            words_new, counter_new = parse_xml(path, words, counter)
+            words = words_new
+            counter = counter_new
+
+    for path in pathlib.Path("Writings").iterdir(): ##ktuvim
+        if path.is_file():
+            words_new, counter_new = parse_xml(path, words, counter)
+            words = words_new
+            counter = counter_new
+
+    f = open('bible_words.csv', 'w', encoding="utf8") ##writing to csv file
+    writer = csv.writer(f)
+    for key, value in words.items():
+        writer.writerow([key, value])
+    f.close()
+
+
+
+
